a3c159a57202285ef8328a5d562e16b66cbc7ea9
maia grotepass <maiatoday@gmail.com>
2021-07-25 20:40:17 +0200

    swap in compose in one  fragment

--- app/src/main/java/net/maiatoday/hellocameraxcompose/CameraComposeFragment.kt
+++ app/src/main/java/net/maiatoday/hellocameraxcompose/CameraComposeFragment.kt
@@ -5,40 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
 import androidx.navigation.fragment.findNavController
-import net.maiatoday.hellocameraxcompose.databinding.FragmentCameraComposeBinding
+import net.maiatoday.hellocameraxcompose.ui.theme.HelloCameraXComposeTheme
 
 /**
  * A simple [Fragment] subclass To hold the compose version of the camera implementaion
  */
 class CameraComposeFragment : Fragment() {
 
-    private var _binding: FragmentCameraComposeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentCameraComposeBinding.inflate(inflater, container, false)
-        return binding.root
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonComposePrev.setOnClickListener {
-            findNavController().navigate(R.id.action_ComposeFragment_to_FirstFragment)
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                HelloCameraXComposeTheme() {
+                    Column {
+                        Text(text = "Hello world.")
+                        Button(onClick = {
+                            // TODO: 2021/07/25  take photo stuff here
+                        }) {
+                            Text(text = getString(R.string.take_photo))
+                        }
+                        Button(onClick = {
+                            findNavController().navigate(R.id.action_ComposeFragment_to_FirstFragment)
+                        }) {
+                            Text(text = getString(R.string.previous))
+                        }
+                    }
+                }
+            }
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
